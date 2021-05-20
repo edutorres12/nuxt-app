3fe4fb23df5f423e40ebd127464363c070240189
Eduardo Torres <eduk_1012@hotmail.com>
2021-05-19 23:04:56 -0500

    Initial Commmit

---
+++ client/components/LargeCardDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <h4>{{cardsSection.title}}</h4>
+        <p class="snippet">
+            {{cardsSection.snippet}}
+        </p>
+        <div class="cards-container">
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['cardsSection']
+    }
+</script>
+
+<style lang="sass" scoped>
+.container 
+	margin-top: 2rem
+
+.header 
+	font-weight: 700 
+	font-size: 1.5rem 
+.snippet 
+	color: grey 
+	margin-bottom: 1.5rem 
+.cards-container 
+	display: flex 
+	justify-content: space-between 
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+	<Hero />
+	<LargeCardDisplay 
+		v-for="cardInfo in LargeCardInfo"
+		:key="cardInfo.id"
+		:cardsSection="cardInfo"
+		/>
+
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from "@/assets/data.js"
+export default {
+        data() {
+            return {
+             LargeCardInfo:  largeCardSections 
+            }
+        },	
+}
+</script>
+
+
+<style>
+
+</style>
